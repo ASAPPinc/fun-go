32386f8859e47a9825d0c1db4165411e2aec825d
rliuAsapp <rliu@asapp.com>
2019-08-09 16:25:29 -0400

    [CB-3008] Setting max idle to be half of max connection (#9)
    * Update ShardSet.go
    
    * Set connection idle time

--- sql/ShardSet.go
+++ sql/ShardSet.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/marcuswestin/fun-go/errs"
 	"github.com/marcuswestin/fun-go/random"
@@ -133,7 +134,8 @@ func newShard(s *ShardSet, dbName string, autoIncrementOffset int) (*Shard, errs
 	}
 
 	db.SetMaxOpenConns(s.maxConns)
-	// db.SetMaxIdleConns(n)
+	db.SetMaxIdleConns(s.maxConns / 2)
+	db.SetConnMaxLifetime(30 * time.Minute)
 	stdErr := db.Ping()
 	if stdErr != nil {
 		return nil, errs.Wrap(stdErr, nil)
